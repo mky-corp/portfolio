36846ed92ee4505126c451e4811271577d8420c4
Brayan <joan300501@gmail.com>
2024-07-19 11:29:45 -0500

    contact form bind with a sheet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-
+import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server",
+  adapter: netlify(),
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
